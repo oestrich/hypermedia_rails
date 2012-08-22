5ca7ff1e7fc588e825be4031ef4588de15d960a6
Eric Oestrich <eric@oestrich.org>
2012-08-22 18:55:37 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hypermedia::Application.config.secret_token = '9f7c24251abf5c0a408b4c3a0e0c771e965340b858a3db59553e7f53d9680c551d917dbf24fd616e1126ee4f4fe097d75f5cc085abc2ffae8e7daf7b4bdca232'
